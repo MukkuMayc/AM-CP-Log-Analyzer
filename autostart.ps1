2b8792006d05401d6f23429fa00e8c2180607ded
Евгений <38925021+JasonPV@users.noreply.github.com>
2019-11-29 20:25:39 +0300

    add auto start

---
+++ autostart.ps1
@@ -0,0 +1,8 @@
+﻿#создаем расписание запуска (ежедневно в полпятого вечера, в течении 10 дней)
+$t = New-JobTrigger -Daily -At 4:30PM -DaysInterval 10
+#сохраняем в переменной учетные данные
+$cred = Get-Credential ivan.admin
+#В качестве опции указываем запуск задания с повышенными привилегиями
+$o = New-ScheduledJobOption -RunElevated
+#регистрируем задание с именем GetLogs
+Register-ScheduledJob -Name GetLogs -FilePath C:\Users\ivan.admin\Desktop\GetLogs.ps1 -Trigger $t -Credential $cred -ScheduledJobOption $o
